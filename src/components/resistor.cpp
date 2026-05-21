3c7d484276d0f09fe9e8ca3b07546c1d45b765f6
PatrikHusar <patrik.husar.sk@gmail.com>
2026-05-21 15:14:37 +0200

    calculation fixes

--- src/components/resistor.cpp
+++ src/components/resistor.cpp
@@ -3,7 +3,7 @@
 float Resistor::testResistor(uint8_t digiPin1, uint8_t digiPin2, uint8_t analogPin, float VCC, float resistor1Value)
 {
     setPinMode(digiPin1, OUTPUT, digiPin2, OUTPUT);
-    setPinValues(digiPin1, HIGH, digiPin2, LOW);
+    setPinValues(digiPin1, LOW, digiPin2, HIGH);
     delay(50);
     float voltage = readAnalogPin(analogPin, VCC);
     return resistor1Value * (voltage / (VCC - voltage));
--- src/components/resistor.h
+++ src/components/resistor.h
@@ -6,6 +6,7 @@
 
 class Resistor : public baseComponent
 {
-    float testResistor(uint8_t digiPin1, uint8_t digiPin2, uint8_t analogPin, float VCC, float resistor1Value);
+    public:
+        float testResistor(uint8_t digiPin1, uint8_t digiPin2, uint8_t analogPin, float VCC, float resistor1Value);
 };
 #endif
