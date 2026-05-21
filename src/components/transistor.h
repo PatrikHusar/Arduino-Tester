447635cb43d4ff587d8a710fbadeb2b1bd83742f
PatrikHusar <patrik.husar.sk@gmail.com>
2026-05-21 14:37:35 +0200

    add missing header guards in transistor.h

--- src/components/transistor.h
+++ src/components/transistor.h
@@ -1,3 +1,6 @@
+#ifndef transistor_H
+#define transistor_H
+
 #include <Arduino.h>
 #include "diode.h"
 
@@ -13,3 +16,4 @@ class Transistor : public baseComponent
     public:
         transistorStatus testTransistor(uint8_t digiPin1, uint8_t digiPin2, uint8_t digiPin3, uint8_t analogPin, float VCC, float tolerance = 0.2, float transistorTOpenVoltageDrop = 0.7);
 };
+#endif
