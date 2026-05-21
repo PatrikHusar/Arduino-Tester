0645b2bfed2b5bd604c9ed2f5e6d3ebed37b60f5
PatrikHusar <patrik.husar.sk@gmail.com>
2026-05-21 16:11:53 +0200

    fixing transistor.cpp variable name

--- src/components/transistor.cpp
+++ src/components/transistor.cpp
@@ -1,25 +1,25 @@
 #include "transistor.h"
 
-transistorStatus Transistor::testTransistor(uint8_t digiPin1, uint8_t emmiterPin, uint8_t digiPin3, uint8_t analogPin, float VCC, float tolerance, float transistorTOpenVoltageDrop)
+transistorStatus Transistor::testTransistor(uint8_t digiPin1, uint8_t basePin, uint8_t digiPin3, uint8_t analogPin, float VCC, float tolerance, float transistorTOpenVoltageDrop)
 {
     Diode diode;
     setPinMode(digiPin3, INPUT);
-    DiodeStatus statusEmmiterPin1 = diode.testDiode(emmiterPin, digiPin1, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
+    DiodeStatus statusbasePin1 = diode.testDiode(basePin, digiPin1, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
     setPinMode(digiPin1, INPUT);
-    DiodeStatus statusEmmiterPin3 = diode.testDiode(emmiterPin, digiPin3, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
-    Serial.println("statusEmmiterPin2: " + String(statusEmmiterPin1) + ", statusEmmiterPin3: " + String(statusEmmiterPin3));
-    if (statusEmmiterPin1 == DIODE_INSERTED_A_C &&
-        statusEmmiterPin3 == DIODE_INSERTED_A_C)
+    DiodeStatus statusbasePin3 = diode.testDiode(basePin, digiPin3, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
+    Serial.println("statusbasePin2: " + String(statusbasePin1) + ", statusbasePin3: " + String(statusbasePin3));
+    if (statusbasePin1 == DIODE_INSERTED_A_C &&
+        statusbasePin3 == DIODE_INSERTED_A_C)
     {
         return TRANSISTOR_INSERTED_NPN;
     }
-    else if (statusEmmiterPin1 == DIODE_INSERTED_C_A &&
-             statusEmmiterPin3 == DIODE_INSERTED_C_A)
+    else if (statusbasePin1 == DIODE_INSERTED_C_A &&
+             statusbasePin3 == DIODE_INSERTED_C_A)
     {
         return TRANSISTOR_INSERTED_PNP;
     }
-    else if (statusEmmiterPin1 == DIODE_NOT_INSERTED &&
-             statusEmmiterPin3 == DIODE_NOT_INSERTED)
+    else if (statusbasePin1 == DIODE_NOT_INSERTED &&
+             statusbasePin3 == DIODE_NOT_INSERTED)
     {
         return TRANSISTOR_NOT_INSERTED;
     }
