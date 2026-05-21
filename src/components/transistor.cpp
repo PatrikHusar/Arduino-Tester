06b1e6aab752d1499b2954b8846a33b10d067ad0
PatrikHusar <patrik.husar.sk@gmail.com>
2026-05-21 13:55:40 +0200

    renaming some variables

--- src/components/transistor.cpp
+++ src/components/transistor.cpp
@@ -4,22 +4,22 @@ transistorStatus Transistor::testTransistor(uint8_t digiPin1, uint8_t emmiterPin
 {
     Diode diode;
     setPinMode(digiPin3, INPUT);
-    DiodeStatus statusPin1Emmiter = diode.testDiode(emmiterPin, digiPin1, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
+    DiodeStatus statusEmmiterPin1 = diode.testDiode(emmiterPin, digiPin1, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
     setPinMode(digiPin1, INPUT);
-    DiodeStatus statusPin3Emmiter = diode.testDiode(emmiterPin, digiPin3, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
-    Serial.println("statusPin1Emmiter: " + String(statusPin1Emmiter) + ", statusPin3Emmiter: " + String(statusPin3Emmiter));
-    if (statusPin1Emmiter == DIODE_INSERTED_A_C &&
-        statusPin3Emmiter == DIODE_INSERTED_A_C)
+    DiodeStatus statusEmmiterPin3 = diode.testDiode(emmiterPin, digiPin3, analogPin, VCC, tolerance, transistorTOpenVoltageDrop);
+    Serial.println("statusEmmiterPin2: " + String(statusEmmiterPin1) + ", statusEmmiterPin3: " + String(statusEmmiterPin3));
+    if (statusEmmiterPin1 == DIODE_INSERTED_A_C &&
+        statusEmmiterPin3 == DIODE_INSERTED_A_C)
     {
         return TRANSISTOR_INSERTED_NPN;
     }
-    else if (statusPin1Emmiter == DIODE_INSERTED_C_A &&
-             statusPin3Emmiter == DIODE_INSERTED_C_A)
+    else if (statusEmmiterPin1 == DIODE_INSERTED_C_A &&
+             statusEmmiterPin3 == DIODE_INSERTED_C_A)
     {
         return TRANSISTOR_INSERTED_PNP;
     }
-    else if (statusPin1Emmiter == DIODE_NOT_INSERTED &&
-             statusPin3Emmiter == DIODE_NOT_INSERTED)
+    else if (statusEmmiterPin1 == DIODE_NOT_INSERTED &&
+             statusEmmiterPin3 == DIODE_NOT_INSERTED)
     {
         return TRANSISTOR_NOT_INSERTED;
     }
