1cc7a4191d5cc0e6ad1b584b22d00201067c8ced
PatrikHusar <patrik.husar.sk@gmail.com>
2026-06-02 18:32:44 +0200

    cleaning testComponent function to baseComponent

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ Button acceptButton;
 Diode diode;
 Transistor transistor;
 Resistor resistor;
-uint8_t pins[3] = {2, 3, 4};
+uint8_t pins[4] = {2, 3, 4, 5};
 uint8_t analogPins[3] = {A2, A1, A0};
 uint8_t switchButtonPin = 13;
 uint8_t acceptButtonPin = 12;
@@ -25,44 +25,8 @@ uint8_t accept = 0;
 unsigned long lastMeasuredTime = 0;
 uint8_t lastSwitchMode = 0;
 
-void testComponent(uint8_t mode, String &text1, String &text2)
-{
-  switch (mode)
-  {
-    case 0: 
-    {
-      Transistor::pinPos transistorElectrodesPos[3];
-      transistorStatus tStatus = transistor.testTransistor(pins, analogPins, VCC, transistorElectrodesPos);
-      text1 = "type: " + String(Transistor::statusToText(tStatus));
-      text2 = "pins: " + String(transistorElectrodesPos[0].name) + " : " 
-                        + String(transistorElectrodesPos[1].name) + " : " 
-                        + String(transistorElectrodesPos[2].name);
-      break;
-    }
-    case 1: 
-    {
-      diodeStatus dStatus = diode.testDiode(pins, analogPins[0], VCC);
-      text1 = "type: " + String(Diode::statusToText(dStatus));
-      text2 = "";
-      break;
-    }
-    case 2: 
-    {
-      float resistorValue = resistor.testResistor(pins, analogPins[0], VCC, 1000.0);
-      text1 = "resistance: ";
-      text2 = resistor.formatResistorValue(resistorValue);
-      break;
-    }
-    default: 
-      text1 = "unknown mode";
-      text2 = "";
-      break;
-  }
-}
-
 void setup()
 {
-  Serial.begin(9600);
   switchButton.init(switchButtonPin);
   acceptButton.init(acceptButtonPin);
 }
@@ -81,7 +45,7 @@ void loop()
     if (millis() - lastMeasuredTime > 1000)
     {
       lastMeasuredTime = millis();
-      testComponent(switchMode, text1, text2);
+      baseComponent::testComponent(switchMode, text1, text2, &transistor, &diode, &resistor, pins, analogPins, VCC);
       Display::displayText(text1, text2, switchMode, display);
     }
   }
