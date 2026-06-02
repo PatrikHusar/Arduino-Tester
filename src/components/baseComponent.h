57c6916d1ea1ca369a6da824cbb7d5bcc48ab2aa
PatrikHusar <patrik.husar.sk@gmail.com>
2026-06-02 17:55:05 +0200

    deleted declaration

--- src/components/baseComponent.h
+++ src/components/baseComponent.h
@@ -7,7 +7,6 @@ class baseComponent
 {
 public:
     static const char* modeToStr(uint8_t mode);
-    static const char* modeToFunction(uint8_t mode);
     static void setPinMode(uint8_t pin1, uint8_t mode1, uint8_t pin2 = 255, uint8_t mode2 = 255, uint8_t pin3 = 255, uint8_t mode3 = 255);
     static void setPinValues(uint8_t pin1, uint8_t value1, uint8_t pin2 = 255, uint8_t value2 = 255, uint8_t pin3 = 255, uint8_t value3 = 255);
     static float readAnalogPin(uint8_t pin, uint8_t vcc, float conversionFactor = 1023.0);
