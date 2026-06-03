464df9d2fff313e452f509b26ea03e0c5fbf6382
PatrikHusar <patrik.husar.sk@gmail.com>
2026-06-03 16:35:44 +0200

    switching resistor pin setting to correct state

--- src/components/resistor.cpp
+++ src/components/resistor.cpp
@@ -3,7 +3,7 @@
 float Resistor::testResistor(uint8_t pins[2], uint8_t analogPin, float VCC, float resistor1Value)
 {
     setPinMode(pins[0], OUTPUT, pins[1], OUTPUT);
-    setPinValues(pins[0], LOW, pins[1], HIGH);
+    setPinValues(pins[0], HIGH, pins[1], LOW);
     delay(50);
     float voltage = readAnalogPin(analogPin, VCC);
     return resistor1Value * (voltage / (VCC - voltage));
